ca6716bbae1348b9470ec25a0a05a7b844eae28c
Bita Azari <bita.az94@gmail.com>
2025-06-03 13:42:13 -0700

    indention

--- selection/js/experiment.js
+++ selection/js/experiment.js
@@ -13,25 +13,25 @@ const fetchVideos = {
 
 const chooseVideoTrial = {
     timeline: [
-      {
+        {
         timeline: [ selecting_one_video ],
         conditional_function: () => {
-          const one = nextTwoVideos.videos.length === 1;
-          if (one) console.log("One video selected");
-          return one;        // ← return the boolean!
+            const one = nextTwoVideos.videos.length === 1;
+            if (one) console.log("One video selected");
+            return one;        // ← return the boolean!
         }
-      },
-      {
+        },
+        {
         timeline: [ selecting_videos ],
         conditional_function: () => {
-          const two = nextTwoVideos.videos.length > 1;
-          if (two) console.log("Two videos selected");
-          return two;       // ← return the boolean!
+            const two = nextTwoVideos.videos.length > 1;
+            if (two) console.log("Two videos selected");
+            return two;       // ← return the boolean!
         }
-      }
+        }
     ]
-  };
-  
+    };
+
 const videoLoop = {
     timeline: [ fetchVideos, chooseVideoTrial ],
     loop_function: () => {

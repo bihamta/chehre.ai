b3426d0814bb544312fb471f2630a34715307a5a
Bita Azari <bita.az94@gmail.com>
2025-12-01 13:28:57 -0800

    down

--- perceive/js/down.js
+++ perceive/js/down.js
@@ -1,7 +1,7 @@
 var down = {
 type: jsPsychInstructions,
 pages: [
-"Sorry! We are currently updating the study. Please check back in a few hours.\nIf you experience any issues, feel free to contact us at bazari@sfu.ca."
+"Sorry! We are currently updating the study. Please check back in a few (~2) hours.\nIf you experience any issues, feel free to contact us at bazari@sfu.ca."
 ],
 show_clickable_nav: false
 };
